dd59a8167f72ce40624790e8a8dbb299f5b5efdf
pastak <pasta0915@gmail.com>
2019-09-11 11:19:02 +0900

    add marquee

---
+++ 2019/main.js
@@ -0,0 +1,26 @@
+'use strict'
+;(() => {
+    const updateLeftTimeElement = document.getElementById('leftTime')
+    const updateLeftTime = () => {
+      const startTime = Date.parse('2019/09/15 16:00+09:00')
+      const leftTime = Math.floor((startTime - Date.now()) / 1000)
+      if (leftTime < 0) {
+        updateLeftTimeElement.textContent = '!!!!!! 現在開催中!今すぐ会場へGo !!!!!!'
+        updateLeftTimeElement.scrollAmount = 40
+        return
+      }
+
+      let t = null
+
+      const days = Math.floor(leftTime / (60 * 60 * 24))
+      t = leftTime % (60 * 60 * 24)
+      const hour = Math.floor(t / (60 * 60))
+      t = t % (60 * 60)
+      const min = Math.floor(t / 60)
+      const sec = t % 60
+
+      updateLeftTimeElement.textContent = `開催まで残り${days}日${hour}時間${min}分${sec}秒!!!!!!!!!!`
+      window.requestAnimationFrame(updateLeftTime)
+    }
+    updateLeftTime()
+})()
